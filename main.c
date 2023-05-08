04f8d24613ad9aaea452fbe8b3443910b4f63b53
jvos-cs <joaovitordeos@gmail.com>
2023-05-08 00:56:14 -0300

    feat

--- main.c
+++ main.c
@@ -2,23 +2,25 @@
 #include "libavl.h"
 
 int main (int argc, char **argv){
+    no_t *raiz;
     int chave;
     char op;
-    no_t *raiz;
 
     raiz = NULL;
-
-    scanf("%c %d", &op, &chave);
-    raiz = criaNo(chave);
     
-    while (chave){
+    while (scanf("%c %d", &op, &chave)){
+        if (raiz == NULL)
+            raiz = criaNo(chave);
+        else{ 
+            if (op == 'i')  insereNo(raiz, chave);
+            if (op == 'r')  raiz = removeNo(buscaNo(raiz, chave), raiz);
+        }
+
+        printf("Operacao %c, no: %d\n", op, chave);
+        if (raiz->esq && raiz->dir) printf("Raiz: %d, fDir: %d, fEsq: %d\n", raiz->chave, raiz->dir->chave, raiz->esq->chave);
+        else printf("Raiz: %d\n", raiz->chave);
         imprimeArvore(raiz);
         printf("\n");
-
-        scanf("%c %d", &op, &chave);
-
-        if (op == 'i')  insereNo(raiz, chave);
-        if (op == 'r')  raiz = removeNo(buscaNo(raiz, chave), raiz);
     }
 
     destroiArvore(raiz);
