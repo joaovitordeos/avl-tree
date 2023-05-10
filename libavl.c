fd6e47e4c48c36a4944727057ea9d4036fcae844
jvos-cs <joaovitordeos@gmail.com>
2023-05-10 14:41:48 -0300

    feat

--- libavl.c
+++ libavl.c
@@ -9,7 +9,7 @@
 #define ROT_ESQ 1
 #define ROT_DIR 2
 
-#define max(x, y) (x>y?x:y) 
+#define max(x, y) (x>y?x:y)           // Calcula o máximo entre dois inteiros.
 
 /*  Cria uma struct no_t com o valor chave 'c'.
     
@@ -157,17 +157,19 @@ no_t *removeNo(no_t *n, no_t *noRaiz){
 
 /* Essa função realiza a rotação à esquerda. */
 no_t *rotacaoEsq(no_t *n){
-    if (n == NULL || n->dir == NULL) return n;
+    no_t *t;
 
-    if (n->pai != NULL) {
-        if (n == n->pai->esq) {
+    if (n == NULL || n->dir == NULL) return n;  // Verifica se os nós envolvidos não são nulos. 
+
+    if (n->pai != NULL) {         // Se o pai não é nulo (não é a raiz).
+        if (n == n->pai->esq) {   // Se for o filho esquerdo, 
             n->pai->esq = n->dir;
         } else {
             n->pai->dir = n->dir;
         }
     }
 
-    no_t *t = n->dir;
+    t = n->dir;
     n->dir = t->esq;
     t->esq = n;
 
@@ -204,13 +206,13 @@ no_t *rotacaoDir(no_t *n){
 }
 
 
-/*  Imprime a travessia em ordem crescente. */
-void imprimeArvore(no_t *n, int nivel){
+/*  Imprime na saída padrão e em ordem crescente, as chaves
+    e o nível do seu respectivo nó separados por ','. */
+void imprimeNivelArvore(no_t *n, int nivel){
     if (n != NULL){
-        imprimeArvore(n->esq, nivel + 1);
-        if (n->pai != NULL) printf("No: %2d, Pai: %2d, Altura: %2d, Nivel: %2d\n", n->chave, n->pai->chave, n->altura, nivel);
-        else printf("No: %2d, Pai: N/A, Altura: %2d, Nivel: %2d\n", n->chave, n->altura, nivel);
-        imprimeArvore(n->dir, nivel + 1);
+        imprimeNivelArvore(n->esq, nivel + 1);
+        fprintf(stdout, "%d,%d\n", n->chave, nivel);
+        imprimeNivelArvore(n->dir, nivel + 1);
     }
 }
 
@@ -233,12 +235,12 @@ int corrigeAltura(no_t *n){
     Retorna o valor da diferença entre o a altura dos nós. */
 int verificaBalanceamento(no_t *n){
     if ((n->dir != NULL) && (n->esq != NULL))
-        return (n->dir->altura - n->esq->altura);
+        return (n->dir->altura - n->esq->altura);      // Se o nó possuir 2 filhos, a diferença é entre a altura do (direito - esquerdo).
 
     if (n->dir == NULL)
-        return (-1 * n->esq->altura);
+        return (-1 * n->esq->altura);                  // Se o nó esquerdo for o único filho, então a diferença é dada pela sua altura * -1.            
 
-    return (n->dir->altura);
+    return (n->dir->altura);                           // Se o nó direitor for o único filho, então a diferença é dada pela sua altura.
 }
 
 /*  Essa função verifica qual vai ser o tipo de rotação.
@@ -264,7 +266,6 @@ int verificaRotacao(no_t *n, int blc){
     return 0;
 }
 
-
 /*  Balanceia a árvore. */
 no_t *balanceiaArvore(no_t *n){
     int blc;
@@ -272,21 +273,21 @@ no_t *balanceiaArvore(no_t *n){
     blc = verificaBalanceamento(n);
 
     if (blc > 1){
-        if (verificaRotacao(n, blc) == ROT_ESQ){
+        if (verificaRotacao(n, blc) == ROT_ESQ){  // Verifica se o desbalanceamento à direita será feito uma rotação simples à esquerda.
             n = rotacaoEsq(n);
         }
         else{
-            n->dir = rotacaoDir(n->dir);
+            n->dir = rotacaoDir(n->dir);          // Se não for simples, então realiza a rotação em 'zig-zag'da dir p/ esq.
             n = rotacaoEsq(n);
         }
     }
 
     else if (blc < -1){
-        if (verificaRotacao(n, blc) == ROT_DIR){
+        if (verificaRotacao(n, blc) == ROT_DIR){  // Verifica se o desbalanceamento à esquerda será feito uma rotação simples à direita.
             n = rotacaoDir(n);
         }
         else{
-            n->esq = rotacaoEsq(n->esq);
+            n->esq = rotacaoEsq(n->esq);          // Se não for simples, então realiza a rotação em 'zig-zag' da esq p/ dir.
             n = rotacaoDir(n);
         }
     }
@@ -304,15 +305,15 @@ no_t *balanceiaAteRaiz(no_t *n, no_t *raiz){
 
     proxPai = n->pai;
 
-    while ((proxPai != raiz)){
+    while (proxPai != raiz){               
         corrigeAltura(raiz);
 
-        proxPai = balanceiaArvore(proxPai);
+        proxPai = balanceiaArvore(proxPai);       // Balanceia as subárvore do nó 'n' até um nó antes ao nó 'raiz'
 
         proxPai = proxPai->pai;
     }
 
-    proxPai = balanceiaArvore(proxPai);
+    proxPai = balanceiaArvore(proxPai);           // Balanceia o nó 'raiz'
 
     corrigeAltura(proxPai);
 
@@ -324,7 +325,7 @@ no_t *insereNoAvl(no_t *n, int c){
 
     insereNo(n, c);
 
-    n = balanceiaAteRaiz(buscaNo(n, c), n);
+    n = balanceiaAteRaiz(buscaNo(n, c), n);   // Balanceia a árvore a partir do novo nó inserido na árvore.
 
     return n;
 }
@@ -338,7 +339,7 @@ no_t *removeNoAvl(no_t *n, no_t *noRaiz){
     if (n == NULL) return novaRaiz;
     
 
-    if ((n->dir == NULL) && (n->esq == NULL)){
+    if ((n->dir == NULL) && (n->esq == NULL)){  // Se o nó for uma folha;
         aux = n->pai;
         
         novaRaiz = removeNo(n, novaRaiz);
@@ -348,31 +349,32 @@ no_t *removeNoAvl(no_t *n, no_t *noRaiz){
         novaRaiz = balanceiaAteRaiz(aux, novaRaiz);
     }
     else{ 
-        if((n->dir != NULL) && (n->esq == NULL)){
+        if((n->dir != NULL) && (n->esq == NULL)){   // Se o nó tiver apenas o filho direito.
             aux = n->dir;
 
             novaRaiz = removeNo(n, novaRaiz);
 
             corrigeAltura(novaRaiz);
 
-            novaRaiz = balanceiaAteRaiz(aux, novaRaiz);
+            novaRaiz = balanceiaAteRaiz(aux, novaRaiz); // Balanceia a árvore a partir do filho dir do nó removido.
         }
-        else if((n->dir == NULL) && (n->esq != NULL)){
+        else if((n->dir == NULL) && (n->esq != NULL)){  // Se o nó tiver apenas o filho esquerdo.
             aux = n->esq;
 
             novaRaiz = removeNo(n, novaRaiz);
 
             corrigeAltura(novaRaiz);
 
-            novaRaiz = balanceiaAteRaiz(aux, novaRaiz);
+            novaRaiz = balanceiaAteRaiz(aux, novaRaiz); // Balanceia a árvore a partir do filho esq do nó removido.
         }
-        else {
+        else {                                          // Se o nó tiver os dois filhos .
             aux = antecessorNo(n)->pai;
+
             novaRaiz = removeNo(n, novaRaiz);
 
             corrigeAltura(novaRaiz);
 
-            novaRaiz = balanceiaAteRaiz(aux, novaRaiz);
+            novaRaiz = balanceiaAteRaiz(aux, novaRaiz);  // Balanceia a árvore a partir do nó pai do antecessor do nó removido.
         }
     }
     return novaRaiz;
